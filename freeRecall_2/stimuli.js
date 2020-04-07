8852980a7bb860bde3b095fe93d00eb14fb6b6a8
mklab <mk@human.kj.yamagata-u.ac.jp>
2020-04-07 17:38:52 +0900

    1st

---
+++ freeRecall_2/stimuli.js
@@ -0,0 +1,22 @@
+//学習語を配列で入力してください
+//シングルクォーテーション(')で囲んでください
+//単語同士はカンマで繋いでください
+let learningWordList = [
+'リンゴ',
+'ミカン',
+'スイカ',
+'イチゴ',
+'メロン',
+'キウイ'
+]
+
+//連想配列に変換
+for (i in learningWordList) {
+    learningWordList[i] = {
+        item: learningWordList[i],
+        itemType: 'learningWordList'
+    }
+}
+
+//ランダマイズ
+learningWordList = jsPsych.randomization.shuffle(learningWordList)
